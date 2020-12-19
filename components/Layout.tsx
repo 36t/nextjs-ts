5d58ad168999865904c6f18e3c1264efa684c53c
36t <saluta@gmail.com>
2020-12-19 23:27:05 +0900

    feat: added return type on function.

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,7 +7,7 @@ type Props = {
   title?: string
 }
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
+const Layout: React.FC = ({ children, title = 'This is the default title' }: Props) => (
   <div>
     <Head>
       <title>{title}</title>
